09f61acbadcf49205f1797563912ab1f5fe764ff
Sam-commit <iit2020264@iiita.ac.in>
2022-03-26 19:41:36 +0530

    homepage

--- Academic_activities/lib/main.dart
+++ Academic_activities/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage()
-    );
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
   }
 }
 
@@ -31,9 +31,84 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home",style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.black),
+        ),
         elevation: 0,
         backgroundColor: Colors.white.withOpacity(0),
+        actions: [
+          Row(
+            children: [
+              Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Icon(
+                Icons.notifications,
+                color: Colors.black,
+              )
+            ],
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Categories()
+        ],
+      ),
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  const Categories({Key? key}) : super(key: key);
+
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Hackathons"),
+          Container(
+            height: 200,
+            width: 300,
+            child: ListView(
+              //scrollDirection: Axis.horizontal,
+              children: [
+                Card()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 200,
+      child: Column(
+        children: [
+          const Expanded(
+            flex: 1,
+            child: Image(
+                image: NetworkImage(
+                    "https://s3.envato.com/files/dc5752b4-e216-4523-9302-aa35bb5c0566/inline_image_preview.jpg"),),
+          ),
+          Expanded(child: Container(child: Text("Hello"),),flex: 1,),
+        ],
       ),
     );
   }
