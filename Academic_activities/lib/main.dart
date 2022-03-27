c6bf3394a322f652f355d5179376aa456a6473bc
Sam-commit <iit2020264@iiita.ac.in>
2022-03-27 12:06:36 +0530

    Drawer added

--- Academic_activities/lib/main.dart
+++ Academic_activities/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'widgets/categories.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,108 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Color(0xFF5264C6),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("images/img.jpg"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Samarth Goel",
+                        style: TextStyle(color: Colors.white,fontSize: 18),
+                      ),
+                      Text("+91 9696969696",style: TextStyle(color: Colors.white,fontSize: 18),)
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.trophy,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Technical Fests",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.code,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Hackathons",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.newspaper,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Scholarships",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.briefcase,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Internships",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.gear,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Settings",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.headset,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Ask a Query",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.circleInfo,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("About Us",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,bottom: 20),
+              child: Row(children: [
+                FaIcon(FontAwesomeIcons.rightFromBracket,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Logout",style: TextStyle(color: Colors.white,fontSize: 18,),)
+              ],),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
         title: Text(
           "Home",
           style: TextStyle(color: Colors.black),
@@ -59,6 +161,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-
